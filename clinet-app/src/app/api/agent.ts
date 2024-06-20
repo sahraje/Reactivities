4d9f19032ac91cf67c9c5501aa36413a9a786756
sahraje <sahrajesh214@gmail.com>
2024-06-20 10:52:46 +0530

    End Of Section Client side user login and registration

--- clinet-app/src/app/api/agent.ts
+++ clinet-app/src/app/api/agent.ts
@@ -3,6 +3,7 @@ import { Activity } from "../models/activity";
 import { toast } from "react-toastify";
 import { router } from "../router/Routes";
 import { store } from "../stores/store";
+import { User, UserFormValues } from "../models/user";
 
 const sleep = (delay: number) => {
     return new Promise((resolve) => {
@@ -12,14 +13,22 @@ const sleep = (delay: number) => {
 
 axios.defaults.baseURL = "http://localhost:5000/api";
 
+const responseBody = <T>(response: AxiosResponse<T>) => response.data;
+
+axios.interceptors.request.use(config => {
+    const token = store.commonStore.token;
+    if (token && config.headers) config.headers.Authorization = `Bearer ${token}`;
+    return config;
+})
+
 axios.interceptors.response.use(async response => {
     await sleep(1000);
     return response;
 }, (error: AxiosError) => {
     const { data, status, config } = error.response as AxiosResponse;
-    switch(status){
+    switch (status) {
         case 400:
-            if(config.method==='get' && data.errors.hasOwnProperty('id')){
+            if (config.method === 'get' && data.errors.hasOwnProperty('id')) {
                 router.navigate('not-found');
             }
             if (data.errors) {
@@ -30,7 +39,7 @@ axios.interceptors.response.use(async response => {
                     }
                 }
                 throw modelStateErrors.flat();
-            }else{
+            } else {
                 toast.error(data);
             }
             break;
@@ -46,12 +55,12 @@ axios.interceptors.response.use(async response => {
         case 500:
             store.commonStore.setServerError(data);
             router.navigate('/server-error')
-            break;            
+            break;
     }
-return Promise.reject(error);
+    return Promise.reject(error);
 });
 
-const responseBody = <T>(response: AxiosResponse<T>) => response.data;
+
 
 const requests = {
     get: <T>(url: string) => axios.get<T>(url).then(responseBody),
@@ -68,8 +77,15 @@ const Activities = {
     delete: (id: string) => requests.del<void>(`/activities/${id}`)
 }
 
+const Account = {
+    current: () => requests.get<User>('/account'),
+    login: (user: UserFormValues) => requests.post<User>('/account/login', user),
+    register: (user: UserFormValues) => requests.post<User>('/account/register', user)
+}
+
 const agent = {
-    Activities
+    Activities,
+    Account
 }
 
 export default agent;
--- clinet-app/src/app/layout/App.tsx
+++ clinet-app/src/app/layout/App.tsx
@@ -1,16 +1,31 @@
-import { Fragment } from 'react';
+import { Fragment, useEffect } from 'react';
 import { Container } from 'semantic-ui-react';
 import NavBar from './NavBar';
 import { observer } from 'mobx-react-lite';
 import { Outlet, useLocation } from 'react-router-dom';
 import HomePage from '../../features/Home/HomePage';
 import { ToastContainer } from 'react-toastify';
+import { useStore } from '../stores/store';
+import LoadingComponent from './LoadingComponent';
+import ModalContainer from '../common/modal/ModalContainer';
 
 function App() {
   const location = useLocation();
+  const { commonStore, userStore } = useStore();
+
+  useEffect(() => {
+    if (commonStore.token) {
+      userStore.getUser().finally(() => commonStore.setAppLoaded())
+    } else {
+      commonStore.setAppLoaded()
+    }
+  }, [commonStore, userStore])
+
+  if (!commonStore.appLoaded) return <LoadingComponent content='Loding app...' />
 
   return (
     <Fragment>
+      <ModalContainer />
       <ToastContainer hideProgressBar position='bottom-right' theme='colored' />
       {location.pathname === '/' ? <HomePage /> : (
         <>
